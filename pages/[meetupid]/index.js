fc02833a087e60bd6160f541483d1cd21a6d096c
rorymcdonnell <rorymcdonnell95@gmail.com>
2021-08-22 19:30:20 +0100

    added page routing

---
+++ pages/[meetupid]/index.js
@@ -0,0 +1,15 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://cdn.pixabay.com/photo/2014/11/13/23/34/palace-530055_960_720.jpg"
+      title="Our first protest"
+      address="Houses of Parliament, London"
+      description="We're going to protest the government until they do the good politics"
+      alt="london meetup"
+    />
+  );
+}
+
+export default MeetupDetails;
---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import MeetupList from "../components/meetups/MeetupList";
+import { useEffect, useState } from "react";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "First Meetup",
+    image:
+      "https://cdn.pixabay.com/photo/2014/11/13/23/34/palace-530055_960_720.jpg",
+    address: "Houses of parliament",
+    description: "protest government",
+  },
+  {
+    id: "m2",
+    title: "Second Meetup",
+    image:
+      "https://cdn.pixabay.com/photo/2014/11/13/23/34/palace-530055_960_720.jpg",
+    address: "Houses of parliament",
+    description: "protest government again",
+  },
+];
+
+function HomePage() {
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    // send a http request and fetch data
+    setLoadedMeetups(DUMMY_MEETUPS);
+  }, []);
+
+  return <MeetupList meetups={loadedMeetups} />;
+}
+
+export async function getStaticProps() {}
+
+export default HomePage;
